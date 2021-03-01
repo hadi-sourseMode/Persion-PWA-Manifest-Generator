a6c02a89644b8b1f09ceed1fcfbccfa9388dadf0
mohammad hadi arefi <zeyton.dev@gmail.com>
2021-03-01 23:07:14 +0100

    edit manifest 4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
 
     window.addEventListener('load', function () {
         navigator.serviceWorker
-            .register("/sw.js")
+            .register("/Persion-PWA-Manifest-Generator/sw.js")
             .then(function () {
                 console.log('service worker registered .');
 
